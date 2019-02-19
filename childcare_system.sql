1da77eb372c7cdd30b7853cbbe5d3d4c0c44512e
EaseCen <you@example.com>
2019-02-19 22:38:17 +0800

    Update childcare_system.sql

--- childcare_system.sql
+++ childcare_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- 主机： 127.0.0.1
--- 生成日期： 2019-02-11 06:14:27
+-- 生成日期： 2019-02-19 15:37:26
 -- 服务器版本： 10.1.36-MariaDB
 -- PHP 版本： 7.2.10
 
@@ -43,17 +43,18 @@ CREATE TABLE `posts` (
 INSERT INTO `posts` (`id`, `name`, `text`, `time`, `author`) VALUES
 (1, '标题1', '正文1', '2019-02-01 00:00:00', 'admin'),
 (2, '标题2', '正文2', '2019-02-02 00:00:00', 'test'),
-(3, '标题1', '正文1', '2019-02-05 14:00:02', 'admin'),
-(4, '标题2', '正文2', '2019-02-14 00:00:33', 'test'),
-(5, '标题2', '正文2', '2019-02-03 00:12:33', 'test');
+(3, '标题6', '正文1', '2019-02-05 14:00:02', 'admin'),
+(4, '标题5', 'asdfasdfaasdfa', '2019-02-14 00:00:00', 'test'),
+(5, '标题3', '正文2', '2019-02-03 00:12:33', 'test'),
+(6, 'azxv', 'zvxc', '2019-02-14 00:00:00', 'zxcvzxcv');
 
 -- --------------------------------------------------------
 
 --
--- 表的结构 `repost`
+-- 表的结构 `reposts`
 --
 
-CREATE TABLE `repost` (
+CREATE TABLE `reposts` (
   `id` int(11) NOT NULL,
   `repost_id` int(11) NOT NULL,
   `text` text NOT NULL,
@@ -62,13 +63,14 @@ CREATE TABLE `repost` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- 转存表中的数据 `repost`
+-- 转存表中的数据 `reposts`
 --
 
-INSERT INTO `repost` (`id`, `repost_id`, `text`, `repost_user`, `time`) VALUES
-(1, 1, '回复1', 'test', '2019-02-01 00:00:00'),
-(2, 2, '回复2', 'admin', '2019-02-03 00:00:00'),
-(3, 2, '回复3', 'admin', '2019-02-03 00:00:01');
+INSERT INTO `reposts` (`id`, `repost_id`, `text`, `repost_user`, `time`) VALUES
+(1, 1, '回复111', 'test', '2019-02-01 00:00:00'),
+(2, 3, '回复211', 'admin', '2019-02-03 00:00:00'),
+(3, 4, '回复3', 'admin', '2019-02-03 00:00:01'),
+(4, 1, '回复4', 'admin', '2019-02-03 00:00:01');
 
 -- --------------------------------------------------------
 
@@ -92,7 +94,10 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`id`, `username`, `password`, `address`, `state`, `per`) VALUES
 (1, 'admin', 'admin', 11, 11, 0),
 (2, 'test', 'test', 22, 21, 1),
-(3, 'root', 'root', 11, 11, 1);
+(3, 'root', 'root', 11, 11, 1),
+(4, '111', '111', 111, 111, 0),
+(5, '123', '123', 123, 123, 0),
+(6, '1111', '1111', 111, 11, 0);
 
 --
 -- 转储表的索引
@@ -105,9 +110,9 @@ ALTER TABLE `posts`
   ADD PRIMARY KEY (`id`);
 
 --
--- 表的索引 `repost`
+-- 表的索引 `reposts`
 --
-ALTER TABLE `repost`
+ALTER TABLE `reposts`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -124,19 +129,19 @@ ALTER TABLE `user`
 -- 使用表AUTO_INCREMENT `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
--- 使用表AUTO_INCREMENT `repost`
+-- 使用表AUTO_INCREMENT `reposts`
 --
-ALTER TABLE `repost`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+ALTER TABLE `reposts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- 使用表AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
